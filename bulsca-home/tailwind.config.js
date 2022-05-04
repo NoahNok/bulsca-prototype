eea28fea46440c9cfff446fc5de5689954a1c9a5
Noah Hollowell <noahdhollowell@googlemail.com>
2022-05-04 16:46:33 +0100

    .

---
+++ bulsca-home/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bulsca: '#070660',
+        bulsca_red: '#9e0d06',
+      }
+    },
+  },
+  plugins: [],
+}
